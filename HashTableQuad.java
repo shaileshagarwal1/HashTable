9efcee72ac0ab2b58d4d4271a076dda443cad20a
Mohamed Maklad <40370787+mmaklad@users.noreply.github.com>
2019-08-26 01:01:32 -0400

    Update HashTableQuad.java

--- HashTableQuad.java
+++ HashTableQuad.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg2si4_lab_4;
-
 /**
  *
  * @author moham
  */
+
 public class HashTableQuad {
     public Integer[] table;
     private int maxLoad, size, load;
